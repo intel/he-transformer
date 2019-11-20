5151de4f8873ef97ac1ccdf410029d6166f3a832
Fabian Boemer <fabian.boemer@intel.com>
2019-11-20 13:17:38 -0800

    Trigger to lamnguye/he-transformer branch

--- .ci/jenkins/jenkins-trigger.groovy
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -22,7 +22,8 @@
 // original script -- we only need to provide this new trigger hook.
 //
 
-String JENKINS_BRANCH = "master"
+// String JENKINS_BRANCH = "master"
+String JENKINS_BRANCH = "lamnguye/he-transformer"
 String TIMEOUTTIME = "3600"
 
 // Constants
