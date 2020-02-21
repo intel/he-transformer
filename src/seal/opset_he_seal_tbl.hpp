8e46aa5a0b879335917a06a8f896886f43aea220
Fabian Boemer <fabian.boemer@intel.com>
2020-02-21 08:41:46 -0800

    Fix pragma once

--- src/seal/opset_he_seal_tbl.hpp
+++ src/seal/opset_he_seal_tbl.hpp
@@ -14,8 +14,6 @@
 // limitations under the License.
 //*****************************************************************************
 
-#pragma once
-
 #define ID_SUFFIX(NAME) NAME
 #include "ngraph/opsets/opset0_tbl.hpp"
 NGRAPH_OP(BoundedRelu, op)
