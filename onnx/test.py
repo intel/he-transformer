825ad7c5b3140d380ceca23e0fa2b6718d685109
Fabian Boemer <fabian.boemer@intel.com>
2020-02-27 15:04:12 -0800

    Encrypted model works for onnx cryptonets

--- onnx/test.py
+++ onnx/test.py
@@ -2,38 +2,64 @@ import onnx
 from ngraph_onnx.onnx_importer.importer import import_onnx_model
 import ngraph as ng
 import numpy as np
+import argparse
 
-onnx_protobuf = onnx.load('cryptonets.onnx')
 
-#print('model.graph.input[0]', onnx_protobuf.graph.input[0])
-#onnx_protobuf.graph.input[0].type.tensor_type.shape.dim[0].dim_param = '10'
-#print('model.graph.input[0]', onnx_protobuf.graph.input[0])
+def run(FLAGS):
 
-#print('model.graph.output[0]', onnx_protobuf.graph.output[0])
-#onnx_protobuf.graph.output[0].type.tensor_type.shape.dim[0].dim_param = '10'
-#print('model.graph.output[0]', onnx_protobuf.graph.output[0])
+    onnx_protobuf = onnx.load('cryptonets.onnx')
 
-#onnx.save(onnx_protobuf, 'cryptonets-fixed.onnx')
+    #print('model.graph.input[0]', onnx_protobuf.graph.input[0])
+    #onnx_protobuf.graph.input[0].type.tensor_type.shape.dim[0].dim_param = '10'
+    #print('model.graph.input[0]', onnx_protobuf.graph.input[0])
 
-#onnx_protobuf = onnx.load('cryptonets-fixed.onnx')
+    #print('model.graph.output[0]', onnx_protobuf.graph.output[0])
+    #onnx_protobuf.graph.output[0].type.tensor_type.shape.dim[0].dim_param = '10'
+    #print('model.graph.output[0]', onnx_protobuf.graph.output[0])
 
-#print('onnx_protobuf', onnx_protobuf)
+    #onnx.save(onnx_protobuf, 'cryptonets-fixed.onnx')
 
-ng_function = import_onnx_model(onnx_protobuf)
+    #onnx_protobuf = onnx.load('cryptonets-fixed.onnx')
 
-print(ng_function)
+    #print('onnx_protobuf', onnx_protobuf)
 
-picture = np.ones([16, 28, 28, 1], dtype=np.float32)
+    ng_function = import_onnx_model(onnx_protobuf)
 
-# Run on interpreter
-int_runtime = ng.runtime(backend_name='INTERPRETER')
-int_cryptonets = int_runtime.computation(ng_function)
-int_pred = int_cryptonets(picture)
-print('int_pred', int_pred)
+    print(ng_function)
 
-# Run on he_seal
-he_runtime = ng.runtime(backend_name='HE_SEAL')
-he_runtime.set_config({'Parameter_8': 'packed'})
-he_cryptonets = he_runtime.computation(ng_function)
-he_pred = he_cryptonets(picture)
-print('he_pred', he_pred)
+    picture = np.ones([16, 28, 28, 1], dtype=np.float32)
+
+    # Run on interpreter
+    int_runtime = ng.runtime(backend_name='INTERPRETER')
+    int_cryptonets = int_runtime.computation(ng_function)
+    int_pred = int_cryptonets(picture)
+    print('int_pred', int_pred)
+
+    # Run on he_seal
+    he_runtime = ng.runtime(backend_name='HE_SEAL')
+
+    config = {}
+    if FLAGS.encryption_parameters != '':
+        config['encryption_parameters'] = FLAGS.encryption_parameters
+
+    config['Parameter_8'] = 'encrypt,packed'
+    print('config', config)
+
+    he_runtime.set_config(config)
+    he_cryptonets = he_runtime.computation(ng_function)
+    he_pred = he_cryptonets(picture)
+    print('he_pred', he_pred)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--encryption_parameters",
+        type=str,
+        default="",
+        help=
+        "Filename containing json description of encryption parameters, or json description itself",
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    run(FLAGS)
